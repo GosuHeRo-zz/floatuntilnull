8de802ac00f35e606b996b72c722553063937fd4
Ajay Patel <ajaysp7@Ajays-MacBook-Pro.local>
2018-04-28 20:31:51 -0400

    I changed input to raw_input

--- err0rch3k0rz.py
+++ err0rch3k0rz.py
@@ -1,16 +1,16 @@
 print ("What is your current body weight?")
-body_weight = input()
+body_weight = raw_input()
 print ("So your body weight is, " + body_weight + ", would you like to know your current bmi and if it needs to be managed?")
-response = input()
+response = raw_input()
 if response == "yes":
     print ("Let's get started")
 else:
     print ("Well come back when you're ready for it")
        
 print ("Enter your height")
-height = input()
+height = raw_input()
 print ("So your height is, " + height + " and your weight is, " + body_weight + " correct?")
-response_dos = input()
+response_dos = raw_input()
 if response_dos == "yes":
     print ("ok here is your bmi")
     print (int(body_weight)/int(height))
